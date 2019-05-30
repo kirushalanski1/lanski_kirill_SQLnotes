8078a51f21223313b54e937b0ac9bdb5a1b1af25
kirushalanski1 <47193575+kirushalanski1@users.noreply.github.com>
2019-05-30 13:39:58 -0700

    Finished search method.

--- app/src/main/java/com/example/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         editName = findViewById(R.id.editText_name);
         editNumber = findViewById(R.id.editText_number);
-        editAddress = findViewById(R.id.editText_number);
+        editAddress = findViewById(R.id.editText_address);
 
         myDb = new DatabaseHelper(this);
         Log.d("MyContactApp", "DatabaseHelper: instantiated DatabaseHelper");
@@ -62,6 +62,79 @@ public class MainActivity extends AppCompatActivity {
 
         showMessage("Data", buffer.toString());
 
+    }
+
+    public void searchData(View view) {
+        Cursor res = myDb.getAllData();
+
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            if (!editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")) {// name ONLY
+                if (res.getString(1).equals(editName.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")){//number ONLY
+                if (res.getString(2).equals(editNumber.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){ // address ONLY
+                if (res.getString(3).equals(editAddress.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (!editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")){//name and number ONLY
+                if (res.getString(1).equals(editName.getText().toString()) && res.getString(2).equals(editNumber.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (!editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){//name and address ONLY
+                if (res.getString(1).equals(editName.getText().toString()) && res.getString(3).equals(editAddress.getText().toString())){
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){//number and address ONLY
+                if (res.getString(2).equals(editNumber.getText().toString()) && res.getString(3).equals(editAddress.getText().toString())){
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else {
+                if (res.getString(1).equals(editName.getText().toString()) &&
+                        res.getString(2).equals(editNumber.getText().toString()) &&
+                        res.getString(3).equals(editAddress.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+        }
+
+        showMessage("Data", buffer.toString());
     }
 
     public void showMessage(String title, String message){
